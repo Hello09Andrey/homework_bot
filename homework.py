997fd992edba19cf55619e2b770926db9ebc9bd2
Hello09Andrey <104667768+Hello09Andrey@users.noreply.github.com>
2022-10-16 18:50:14 +0300

    logger name

--- homework.py
+++ homework.py
@@ -13,11 +13,11 @@ from exceptions import HTTPRequestError
 load_dotenv()
 
 
-logger = logging.getLogger()
+logger = logging.getLogger(name=__name__)
 logger.setLevel(logging.DEBUG)
 
 file_handler = logging.FileHandler('main.log')
-file_handler.setLevel(logging.ERROR)
+file_handler.setLevel(logging.INFO)
 
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.INFO)
